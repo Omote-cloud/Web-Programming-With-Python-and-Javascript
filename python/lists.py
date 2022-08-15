8f55062664e6dcb3c1ab7be2b31f98887a602b28
Omote <immanuelomote@gmail.com>
2022-08-15 12:24:15 +0000

    Toying around with lists

---
+++ python/lists.py
@@ -0,0 +1,12 @@
+#This is a python comment
+names = ["Harry", "Ron", "Hermione"]
+#Print entire list
+print(names)
+#Print second element of the list
+print(names[1])
+#Add a new name to the list
+names.append("Draco")
+#Sort the list
+names.sort()
+#Print the new list
+print(names)
