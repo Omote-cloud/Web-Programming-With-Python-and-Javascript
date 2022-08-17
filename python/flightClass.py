c97d5a9b2c41740dd61f02ff8d3fdf86801b14cf
Omote <immanuelomote@gmail.com>
2022-08-17 08:12:58 +0000

    Augmented it to check for number of empty seats

--- python/flightClass.py
+++ python/flightClass.py
@@ -6,5 +6,12 @@ class Flight():
         
     #Method to add passenger to the flight
     def add_passenger(self, name):
+        if not self.open_seats():
+            return False
         self.passengers.append(name)
+        return True
+    
+    #Method to return number of open seats
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
         
