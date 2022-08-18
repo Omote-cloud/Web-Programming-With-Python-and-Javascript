a76ede930a27f8413bbc8d7c7581bd288b67e76d
Omote <immanuelomote@gmail.com>
2022-08-18 07:25:46 +0000

    Division by Zero Exception Handling

--- python/exceptionsOne.py
+++ python/exceptionsOne.py
@@ -1,6 +1,14 @@
+import sys
+
 x = int(input("X: "))
 y = int(input("Y: "))
 
-result = x / y
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: Cannot divide by 0")
+    #Exit the program
+    sys.exit(1)
+    
 
-print(result)
+print(f"{x} / {y} = {result}")
